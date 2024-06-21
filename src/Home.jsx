c95844d6358afcbadbfce2fbe7bcf397728d3404
JilbertVasquez <jilbertvasquez@gmail.com>
2024-06-21 20:01:26 +0800

    some changes and practice

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,11 +3,23 @@ import React from 'react';
 import './custom.css';
 
 function Home () {
+    const smoothScroll = (event) => {
+        event.preventDefault();
+        const element = document.getElementById('Home');
+        if (element) {
+            element.scrollIntoView({
+            behavior: 'smooth'
+            });
+        }
+    };
 
     return (
-        <section className="custom_bg_1 container 3xl h-screen">
-            <nav className='w-full h-5'>
-                <h1 className='custom_font_color_1 text-2xl uppercase font-black'>jv</h1>
+        <section className="custom_bg_1 container 3xl h-screen" id="Home">
+            <nav className='bg-red-800 fixed w-screen h-14 flex justify-center items-center'>
+                <div className='h-10 w-36 bg-white flex justify-center items-center'>
+                    <a href="#Home" className='custom_font_color_1 text-2xl uppercase font-black' onClick={smoothScroll}>JV</a>
+                </div>
+
             </nav>
         </section>
     )
