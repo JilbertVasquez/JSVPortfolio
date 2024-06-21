8a26ee08b3980e6bc33be5316899e2115262df03
JilbertVasquez <jilbertvasquez@gmail.com>
2024-06-21 23:40:38 +0800

    done typewriter

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
-
+import { Typewriter, Cursor } from 'react-simple-typewriter';
 import './custom.css';
 
 function Home () {
+
+    // const {text} = useTypewriter({
+    //     words: ['Computer Engineer', 'Web Developer', "Future Software Engineer"],
+    //     loop: true,
+    //     deleteSpeed: 50,
+    //     typeSpeed: 100,
+    // });
+
     const smoothScroll = (event) => {
         event.preventDefault();
         const element = document.getElementById('Home');
@@ -52,10 +60,17 @@ function Home () {
                     <img className='my-pic rounded-full w-4/5 border-2 border-sky-500 ' src="src/assets/jil1.jpg" alt="jil-pic" />
                 </div>
                 <div className=' h-screen w-3/5 flex justify-start items-center px-20'>
-                    <div className=' h-2/5 w-4/5 custom_bg_1 flex flex-col justify-start items-start px-8'>
-                        <h1 className='text-white uppercase font-medium text-4xl pt-5 pb-1'>Hi I am</h1>
+                    <div className=' h-2/5 w-full custom_bg_1 flex flex-col justify-start items-start px-8'>
+                        <h1 className='text-white uppercase font-medium text-4xl pt-5 pb-1'>Hi, I am</h1>
                         <h1 className='text-white uppercase font-medium text-5xl pt-1 pb-2'>Jilbert S. Vasquez</h1>
-                        <h1 className='custom_font_color_1 uppercase font-semibold tracking-wider text-4xl pt-2 pb-5'>Computer Engineer</h1>
+                        <h1 className='custom_font_color_1 uppercase font-semibold tracking-wider text-4xl pt-2 pb-5'> 
+                            <Typewriter 
+                                loop={true}
+                                words={['Computer Engineer', 'Web Developer', "Future Software Engineer"]}
+                                typeSpeed={250}
+                                delaySpeed={1500}
+                            /><Cursor />
+                        </h1>
                     </div>
                 </div>
             </section>
