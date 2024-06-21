7272775bd5c2762ab0c0bddbd254ebb1f53416f7
JilbertVasquez <jilbertvasquez@gmail.com>
2024-06-21 23:08:41 +0800

    fix width

--- src/Home.jsx
+++ src/Home.jsx
@@ -47,12 +47,12 @@ function Home () {
                     <a href="#Home" className='custom_font_color_1 text-xl uppercase font-semibold hover:border-b-2 text-white' onClick={smoothScroll}>Contact</a>
                 </div>
             </nav>
-            <section className=' h-screen w-full flex'>
+            <section className=' h-screen w-4/5 flex justify-center items-center mx-auto'>
                 <div className=' h-screen w-2/5 flex justify-center items-center'>
-                    <img className='my-pic rounded-full w-9/12 border-2 border-sky-500 ' src="src/assets/jil1.jpg" alt="jil-pic" />
+                    <img className='my-pic rounded-full w-4/5 border-2 border-sky-500 ' src="src/assets/jil1.jpg" alt="jil-pic" />
                 </div>
-                <div className='custom_bg_1 h-screen w-3/5 flex justify-start items-center px-20'>
-                    <div className='h-2/5 w-4/5 custom_bg_1 flex flex-col justify-start items-start px-8'>
+                <div className=' h-screen w-3/5 flex justify-start items-center px-20'>
+                    <div className=' h-2/5 w-4/5 custom_bg_1 flex flex-col justify-start items-start px-8'>
                         <h1 className='text-white uppercase font-medium text-4xl pt-5 pb-1'>Hi I am</h1>
                         <h1 className='text-white uppercase font-medium text-5xl pt-1 pb-2'>Jilbert S. Vasquez</h1>
                         <h1 className='custom_font_color_1 uppercase font-semibold tracking-wider text-4xl pt-2 pb-5'>Computer Engineer</h1>
