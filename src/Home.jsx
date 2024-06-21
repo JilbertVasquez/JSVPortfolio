9975f1a418578e14cd22f9e9fb734e2d3ac36ba4
JilbertVasquez <jilbertvasquez@gmail.com>
2024-06-21 22:15:41 +0800

    done nav

--- src/Home.jsx
+++ src/Home.jsx
@@ -14,13 +14,42 @@ function Home () {
     };
 
     return (
-        <section className="custom_bg_1 container 3xl h-screen" id="Home">
-            <nav className='bg-red-800 fixed w-screen h-14 flex justify-center items-center'>
-                <div className='h-10 w-36 bg-white flex justify-center items-center'>
-                    <a href="#Home" className='custom_font_color_1 text-2xl uppercase font-black' onClick={smoothScroll}>JV</a>
+        <section className="con custom_bg_1 container 3xl h-screen" id="Home">
+            <nav className='head custom_bg_1 w-full h-14  columns-8 flex justify-around items-center px-20 z-10'>
+                <div className='h-10 w-36 p-3 custom_bg_1 flex justify-center items-center'>
+                    <a href="#Home" className='custom_font_color_1 text-2xl uppercase font-black hover:border-b-2 text-white' onClick={smoothScroll}>JV</a>
+                </div>
+
+                <div className='h-10 w-10 p-2 custom_bg_1 flex justify-center items-center'>
+                    
+                </div>
+
+                <div className='h-10 w-38 p-2 custom_bg_1 flex justify-center items-center'>
+                    <a href="#Home" className='text-xl text-white uppercase hover:border-b-2 border-sky-500' onClick={smoothScroll}>About Me</a>
+                </div>
+
+                <div className='h-10 w-38 p-2 custom_bg_1 flex justify-center items-center'>
+                    <a href="#Home" className='custom_font_color_1 text-xl uppercase font-semibold hover:border-b-2 text-white' onClick={smoothScroll}>Experience</a>
+                </div>
+
+                <div className='h-10 w-38 p-2 custom_bg_1 flex justify-center items-center'>
+                    <a href="#Home" className=' text-xl uppercase text-white  hover:border-b-2 border-sky-500' onClick={smoothScroll}>Projects</a>
+                </div>
+
+                <div className='h-10 w-38 p-2 custom_bg_1 flex justify-center items-center'>
+                    <a href="#Home" className='custom_font_color_1 text-xl uppercase font-semibold hover:border-b-2 text-white' onClick={smoothScroll}>Skills</a>
+                </div>
+
+                <div className='h-10 w-38 p-2 custom_bg_1 flex justify-center items-center'>
+                    <a href="#Home" className=' text-xl text-white uppercase  hover:border-b-2 border-sky-500' onClick={smoothScroll}>Certificates</a>
+                </div>
+
+                <div className='h-10 w-38 p-2 custom_bg_1 flex justify-center items-center'>
+                    <a href="#Home" className='custom_font_color_1 text-xl uppercase font-semibold hover:border-b-2 text-white' onClick={smoothScroll}>Contact</a>
                 </div>
 
             </nav>
+            
         </section>
     )
 }
