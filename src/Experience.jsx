380332c4b36a2eb19cd1d7364485e3180f666417
JilbertVasquez <jilbertvasquez@gmail.com>
2024-06-23 13:53:55 +0800

    fix experience section

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -5,13 +5,12 @@ import './custom.css';
 function Experience () {
 
     return (
-        <section className='container w-full h-screen custom_bg_1'>
-            <header className='flex justify-center bg-red-500'>
-                <div className='mt-20'>
-                    <h1 className='custom_font_color_1 tracking-wider text-start px-5 p-5 text-3xl uppercase font-semibold ' >Experience</h1>
+        <section className="container custom_bg_1 3xl h-screen flex justify-center items-center " id='Experience'>
+            <section className='w-4/5 h-screen flex bg-slate-600'>
+                <div className='w-full h-2/3 px-16 mt-14 flex justify-center '>
+                    <h1 className='custom_font_color_1 tracking-wider text-start px-5 p-5 text-3xl uppercase font-semibold' >Experience</h1>
                 </div>
-                
-            </header>
+            </section>
         </section>
     )
 }
