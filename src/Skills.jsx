8d07f107612429dc748fadb659cca3e9b7e1068c
JilbertVasquez <jilbertvasquez@gmail.com>
2024-06-24 23:50:52 +0800

    skillset mapping on going

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 
 import './custom.css';
 
+const Skillset = [
+    {
+        title: 'Web Development',
+    },
+    {
+        title: 'Application Development',
+    },
+    {
+        title: 'Tools',
+    },
+    {
+        title: 'Others',
+    },
+];
+
 function Skills () {
 
     return (
@@ -10,8 +25,13 @@ function Skills () {
                 <div className='w-full h-20 px-16 flex justify-center items-center bg-indigo-200 mt-14'>
                     <h1 className='custom_font_color_1 tracking-wider text-start px-5 pt-2 text-3xl uppercase font-semibold' >Skiilsset/TechStack</h1>
                 </div>
-                <EventCardContainer />
-                <EventCardContainer />
+
+                {Skillset.map((skill, index) => (
+                    <EventCardContainer key={index} skillset={skill} />
+                ))}
+
+                {/* <EventCardContainer /> */}
+                {/* <EventCardContainer /> */}
             </section>
             
         </section>
@@ -21,19 +41,22 @@ function Skills () {
 const EventCard = () => {
     return (
         <div className='w-28 flex flex-col p-5 rounded-xl custom_bg_1 border-sky-800 skill_shadow'>
-            <img className='w-full h-full' src="html.png" alt="html" />
+            <img className='w-full h-full' src="logos/html.png" alt="html" />
             <h3 className='text-white uppercase text-lg pt-4'>html</h3>
         </div>
     )
 }
 
-const EventCardContainer = () => {
+const EventCardContainer = ({skillset}) => {
     return (
         <div className='w-full flex flex-col bg-red-100'>
-            <div className='w-full h-14 px-14 flex mt-12 justify-center items-center '>
-                <h2 className='text-white tracking-wider text-start px-5 text-lg uppercase font-semibold' >Web Development</h2>
+            <div className='w-full h-14 px-14 flex mt-10 justify-center items-center '>
+                <h2 className='text-white tracking-wider text-start px-5 text-lg uppercase font-semibold' >{skillset.title}</h2>
+                {/* {skillset.map((skill, index) => (
+                    <h2 key={index} className='text-white tracking-wider text-start px-5 text-lg uppercase font-semibold' >{skill.title}</h2>
+                ))} */}
             </div>
-            <div className='p-8 w-full px-40 pb-12 flex flex-wrap gap-10 justify-center items-center'>
+            <div className='p-8 w-full px-40 pb-8 flex flex-wrap gap-10 justify-center items-center'>
                 <EventCard />
                 <EventCard />
                 <EventCard />
